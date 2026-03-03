b002ef78019ab0a30083f9907810a1a6930af1c9
Chiara BOSCAGLI <c23bosca@sl-tp-br-511.imta.fr>
2026-03-03 14:47:34 +0100

    Restructuration

--- ma copine/densenet.py
+++ ma copine/densenet.py
@@ -1,25 +1,22 @@
+'''DenseNet in PyTorch.'''
 import math
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.ao.quantization as quant
 
 
 class Bottleneck(nn.Module):
     def __init__(self, in_planes, growth_rate):
         super(Bottleneck, self).__init__()
         self.bn1 = nn.BatchNorm2d(in_planes)
-        self.relu1 = nn.ReLU(inplace=True)
         self.conv1 = nn.Conv2d(in_planes, 4*growth_rate, kernel_size=1, bias=False)
         self.bn2 = nn.BatchNorm2d(4*growth_rate)
-        self.relu2 = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(4*growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
 
     def forward(self, x):
-        
-        out = self.conv1(self.relu1(self.bn1(x)))
-        out = self.conv2(self.relu1(self.bn2(out)))
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = self.conv2(F.relu(self.bn2(out)))
         out = torch.cat([out,x], 1)
         return out
 
@@ -28,11 +25,10 @@ class Transition(nn.Module):
     def __init__(self, in_planes, out_planes):
         super(Transition, self).__init__()
         self.bn = nn.BatchNorm2d(in_planes)
-        self.relu = nn.ReLU(inplace=True)
         self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
 
     def forward(self, x):
-        out = self.conv(self.relu(self.bn(x)))
+        out = self.conv(F.relu(self.bn(x)))
         out = F.avg_pool2d(out, 2)
         return out
 
@@ -40,8 +36,6 @@ class Transition(nn.Module):
 class DenseNet(nn.Module):
     def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_classes=10):
         super(DenseNet, self).__init__()
-        self.quant = quant.QuantStub()
-        self.dequant = quant.DeQuantStub()
         self.growth_rate = growth_rate
 
         num_planes = 2*growth_rate
@@ -71,9 +65,6 @@ class DenseNet(nn.Module):
         self.bn = nn.BatchNorm2d(num_planes)
         self.linear = nn.Linear(num_planes, num_classes)
 
-        self.relu = nn.ReLU(inplace=True)
-
-
     def _make_dense_layers(self, block, in_planes, nblock):
         layers = []
         for i in range(nblock):
@@ -82,18 +73,14 @@ class DenseNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.quant(x)
         out = self.conv1(x)
         out = self.trans1(self.dense1(out))
         out = self.trans2(self.dense2(out))
         out = self.trans3(self.dense3(out))
         out = self.dense4(out)
-        out = self.relu(self.bn(out))
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(F.relu(self.bn(out)), 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-
-        out = self.dequant(out)
         return out
 
 def DenseNet121():
